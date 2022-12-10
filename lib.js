bb39b3cafba10d24f9d232df66d274e6f214cd1b
gospeedricer <119834614+gospeedricer@users.noreply.github.com>
2022-12-10 14:22:10 -0800

    Update lib.js

--- lib.js
+++ lib.js
@@ -78,7 +78,7 @@ McIntosh.prototype.init = function(opts, closecb) {
     this.initializing = true;
 
         this._port = new SerialPort(opts.port, {
-            baudRate: 115200,
+            baudRate: opts.baud || 115200
         });
 
         let parser = this._port.pipe(new Readline(")"));
