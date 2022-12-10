2ba05558316bced83795dddcd34a917edf6f7b5d
Carlo Santos <santos.carlo83@gmail.com>
2022-12-10 15:24:30 -0800

    1

--- lib.js
+++ lib.js
@@ -78,7 +78,7 @@ McIntosh.prototype.init = function(opts, closecb) {
     this.initializing = true;
 
         this._port = new SerialPort(opts.port, {
-            baudRate: opts.baud || 115200
+            baudRate: 9600
         });
 
         let parser = this._port.pipe(new Readline(")"));
