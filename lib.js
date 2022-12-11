f66ebcafb9cedf0d7c229786264212e02fa763c0
Carlo Santos <santos.carlo83@gmail.com>
2022-12-10 16:56:47 -0800

    1

--- lib.js
+++ lib.js
@@ -22,7 +22,7 @@ let _processw = function() {
     this._woutstanding = true;
     console.log("[McIntosh] writing:", this._qw[0]);
 
-    this._port.write(this._qw[0] + "\n",
+    this._port.write(this._qw[0] + "\r",
                     (err) => {
                         if (err) return;
                         this._qw.shift();
